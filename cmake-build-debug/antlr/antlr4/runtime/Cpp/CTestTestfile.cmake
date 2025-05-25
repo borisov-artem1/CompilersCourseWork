44736ad3b8ca42eae5e3158da50c11e074e44f93
borisov-artem1 <101502576+borisov-artem1@users.noreply.github.com>
2025-05-25 13:26:28 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/antlr/antlr4/runtime/Cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artemborisov/CLionProjects/Compilers/antlr/antlr4/runtime/Cpp
-# Build directory: /home/artemborisov/CLionProjects/Compilers/cmake-build-debug/antlr/antlr4/runtime/Cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("runtime")
--- cmake-build-debug/antlr/antlr4/runtime/Cpp/runtime/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artemborisov/CLionProjects/Compilers/antlr/antlr4/runtime/Cpp/runtime
-# Build directory: /home/artemborisov/CLionProjects/Compilers/cmake-build-debug/antlr/antlr4/runtime/Cpp/runtime
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/artemborisov/CLionProjects/Compilers/cmake-build-debug/antlr/antlr4/runtime/Cpp/runtime/antlr4_tests[1]_include.cmake")
-subdirs("../../../../../_deps/googletest-build")
