069d4ceabdc8c4195c937d0422e922b22ff7fb3a
borisov-artem1 <artem.borisov2002@mail.ru>
2025-05-25 13:24:42 +0300

    done

--- compiler/rw_files.cpp
+++ compiler/rw_files.cpp
@@ -8,6 +8,79 @@
 #include "oberonParser.h"
 #include "oberonVisitor.h"
 
+#include "json.hpp"
+using json = nlohmann::json;
+
+int nodeCounter = 0;
+std::string escape(const std::string& s)
+{
+    std::string escaped;
+    for (char c : s)
+    {
+        if (c == '"')
+        {
+            escaped += "\\\"";
+        } else
+        {
+            escaped += c;
+        }
+    }
+    return escaped;
+}
+
+int jsonToDot(const json& node, std::ostream& out, int parentId = -1)
+{
+    int current = nodeCounter++;
+    auto label = node["name"].get<std::string>();
+
+    out << " node" << current << " [label=\"" << escape(label) << "\"];\n";
+
+    if (parentId != -1)
+    {
+        out << " node" << parentId << "-> node" << current << ";\n";
+    }
+
+    for (const auto& child : node["children"])
+    {
+        jsonToDot(child, out, current);
+    }
+
+    return current;
+}
+
+void generateDotFile(const json& node, const std::string& filename)
+{
+    std::ofstream out(filename);
+    out << "digraph ParseTree {\n";
+    out << " node [shape=box, fontname=\"Courier\"];\n";
+    jsonToDot(node, out);
+    out << "}\n";
+    out.close();
+}
+
+json parseTreeToJson(antlr4::tree::ParseTree* tree, antlr4::Parser* parser) {
+    json node;
+
+    std::string nodeName = tree->getText();
+    auto ruleIndex = dynamic_cast<antlr4::ParserRuleContext*>(tree);
+    if (ruleIndex) {
+        nodeName = parser->getRuleNames()[ruleIndex->getRuleIndex()];
+    }
+
+    node["name"] = nodeName;
+    // написать типы узлов, так как дерево типизированное
+
+    if (tree->children.empty()) {
+        node["children"] = json::array();
+    } else {
+        for (auto child : tree->children) {
+            node["children"].push_back(parseTreeToJson(child, parser));
+        }
+    }
+
+    return node;
+}
+
 void processing_files(std::ifstream& ifs)
 {
 
@@ -19,6 +92,14 @@ void processing_files(std::ifstream& ifs)
     oberonParser parser(&tokens);
 
     antlr4::tree::ParseTree* tree = parser.module();
+
+    json treeJson = parseTreeToJson(tree, &parser);
+
+    std::ofstream out("parse_tree.json");
+    generateDotFile(treeJson, "tree.dot");
+    system("dot -Tpng tree.dot -o tree.png");
+    out << treeJson.dump(2);
+    out.close();
 
     llvm::LLVMContext llvmContext;
     llvm::IRBuilder<> builder(llvmContext);
